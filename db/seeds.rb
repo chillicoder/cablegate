6f33e7ab5b19c02945e812b7f8571a7f0d220a87
Dave Sag <davesag@gmail.com>
2010-12-08 07:23:38 +1100

    updated to build 201012070806

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,13 +9,15 @@ Time.zone = :utc
 ActiveRecord::Base.time_zone_aware_attributes = true
 ActiveRecord::Base.default_timezone = :utc
 
-puts "Seeding database with Default Mirror"
-
-mirror = Mirror.find_by_name("Default")
+mirror = Mirror.find_by_name("default")
 if mirror != nil
+  puts "Deleting previous default mirror."
   mirror.destroy
 end
-mirror = Mirror.create( :name => "default", :uri => "http://cablegate.heroku.com", :build_number => "201012061721")
+
+puts "Seeding database with default mirror"
+
+mirror = Mirror.create( :name => "default", :uri => "http://cablegate.heroku.com", :build_number => "201012070806")
 mirror.save!
 
 # add any other seeding here.
